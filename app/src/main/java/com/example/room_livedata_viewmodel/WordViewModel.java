a1733de75bb8e5c9429a3d57fdb54587a1782966
LAPTOP <sayantandas657@outlook.com>
2021-06-11 18:48:41 +0530

    Created a view model

---
+++ app/src/main/java/com/example/room_livedata_viewmodel/WordViewModel.java
@@ -0,0 +1,35 @@
+package com.example.room_livedata_viewmodel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
+public class WordViewModel extends AndroidViewModel
+{
+    private final WordRepository wordRepository;
+    private final LiveData<List<Word_Entity>> allWords;
+
+    public WordViewModel(@NonNull @NotNull Application application)
+    {
+        super(application);
+
+        wordRepository = new WordRepository(application);
+        allWords = wordRepository.getAllWordsFromLiveData();
+    }
+
+    LiveData<List<Word_Entity>> getAllWords()
+    {
+        return allWords;
+    }
+
+    void insert (Word_Entity word)
+    {
+        wordRepository.insertWord(word);
+    }
+}
