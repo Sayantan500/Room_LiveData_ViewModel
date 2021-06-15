f726e1e6206ef553b36cfab6c258c5498c212665
LAPTOP <sayantandas657@outlook.com>
2021-06-15 13:12:56 +0530

    added a primary key

--- app/src/main/java/com/example/room_livedata_viewmodel/Word_Entity.java
+++ app/src/main/java/com/example/room_livedata_viewmodel/Word_Entity.java
@@ -8,7 +8,8 @@ import io.reactivex.rxjava3.annotations.NonNull;
 @androidx.room.Entity(tableName = "WORDS_Table")
 class Word_Entity
 {
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
+    private int wordNo;
     @androidx.annotation.NonNull
     @ColumnInfo(name = "word")
     private final String word;
@@ -21,5 +22,13 @@ class Word_Entity
     @org.jetbrains.annotations.NotNull
     public String getWord() {
         return this.word;
+    }
+
+    public int getWordNo() {
+        return wordNo;
+    }
+
+    public void setWordNo(int wordNo) {
+        this.wordNo = wordNo;
     }
 }
